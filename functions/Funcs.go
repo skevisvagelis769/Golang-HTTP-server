e21a52be5b7e4081020ebbe24374631a9ab034b0
skevisvagelis769 <skevisvagelis769@gmail.com>
2025-07-26 19:15:46 +0300

    Added comments to Funcs.go

--- functions/Funcs.go
+++ functions/Funcs.go
@@ -8,16 +8,17 @@ import (
 	"sync"
 )
 
-var userCache = make(map[int]User)
-var cacheMutex sync.RWMutex
+var userCache = make(map[int]User) //Temporary user storage
+var cacheMutex sync.RWMutex        //mutex to block other threads from accessing the userCache during read or write operations since its global
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", 200)
 }
 
+// adds a user to the userCache map
 func SetUser(w http.ResponseWriter, r *http.Request) {
 	var user User
-	err := json.NewDecoder(r.Body).Decode(&user)
+	err := json.NewDecoder(r.Body).Decode(&user) //get body from request (the json we POSTed) and put it into user
 	if err != nil {
 		http.Error(
 			w,
@@ -34,16 +35,17 @@ func SetUser(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
-	cacheMutex.Lock()
+	cacheMutex.Lock() //lock userCache and add user to it
 	userCache[len(userCache)+1] = user
 	cacheMutex.Unlock()
 	w.WriteHeader(http.StatusNoContent)
 
 }
 
+// This returns the user specified by the id in the URL
 func GetUser(w http.ResponseWriter, r *http.Request) {
 
-	id, err := strconv.Atoi(r.PathValue("id"))
+	id, err := strconv.Atoi(r.PathValue("id")) //get the id from the part of the URL that is for the {id}
 	if err != nil {
 		http.Error(
 			w,
@@ -64,7 +66,7 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	j, err := json.Marshal(user)
+	j, err := json.Marshal(user) //convert the user we found earlier into json
 	if err != nil {
 		http.Error(
 			w,
@@ -74,9 +76,10 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	w.Write(j)
+	w.Write(j) //write the json to the w writer
 }
 
+// This deletes the user based on the {id} specified in the url
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
@@ -88,7 +91,7 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	cacheMutex.RLock()
-	_, ok := userCache[id]
+	_, ok := userCache[id] //we access the user cache in a mutex lock.We are only reading it here so we only prevent read access
 	cacheMutex.RUnlock()
 	if !ok {
 		http.Error(
@@ -98,11 +101,12 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
-	cacheMutex.Lock()
+	cacheMutex.Lock() //we are reading and writing the userCache so we are completely locking access to it
 	for i := id + 1; i < len(userCache)+1; i++ {
 		userCache[i-1] = userCache[i]
 	}
-	delete(userCache, len(userCache))
+	delete(userCache, len(userCache)) //after we replace the user that we wanted to delete based on the id with the users after them so that theres continuity in the map
+	//we delete the last user in the userCache (so the id equal to its length) because the last and the last - 1 users in the map will be the same!
 
 	cacheMutex.Unlock()
 	w.WriteHeader(http.StatusOK)
