32aa7167ef97b82807210fe4ec1d1f7767da031d
skevisvagelis769 <skevisvagelis769@gmail.com>
2025-07-26 18:38:37 +0300

    Added get user, add user and delete user functionality

--- functions/Funcs.go
+++ functions/Funcs.go
@@ -1,14 +1,109 @@
 package functions
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+var userCache = make(map[int]User)
+var cacheMutex sync.RWMutex
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", 200)
 }
 
 func SetUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hiiii")
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusBadRequest,
+		)
+		return
+	}
+	if user.Name == "" {
+		http.Error(
+			w,
+			"Name is required",
+			http.StatusBadRequest,
+		)
+		return
+	}
+	cacheMutex.Lock()
+	userCache[len(userCache)+1] = user
+	cacheMutex.Unlock()
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusBadRequest,
+		)
+		return
+	}
+	cacheMutex.RLock()
+	user, ok := userCache[id]
+	cacheMutex.RUnlock()
+	if !ok {
+		http.Error(
+			w,
+			"Did not find",
+			http.StatusBadRequest,
+		)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(user)
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusBadRequest,
+		)
+		return
+	}
+	cacheMutex.RLock()
+	_, ok := userCache[id]
+	cacheMutex.RUnlock()
+	if !ok {
+		http.Error(
+			w,
+			"Didnt find",
+			http.StatusBadRequest,
+		)
+		return
+	}
+	cacheMutex.Lock()
+	for i := id + 1; i < len(userCache)+1; i++ {
+		userCache[i-1] = userCache[i]
+	}
+	delete(userCache, len(userCache))
+
+	cacheMutex.Unlock()
+	w.WriteHeader(http.StatusOK)
 }
--- src/App.go
+++ src/App.go
@@ -10,6 +10,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", functions.HandleRoot)
 	mux.HandleFunc("POST /users", functions.SetUser)
+	mux.HandleFunc("GET /users/{id}", functions.GetUser)
+	mux.HandleFunc("DELETE /users/{id}", functions.DeleteUser)
 	fmt.Printf("Server listening to 8080")
 	http.ListenAndServe(":8080", mux)
 }
